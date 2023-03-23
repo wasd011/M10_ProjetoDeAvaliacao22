011063347eca89cf01f3b67824c84c82bf1cbcb8
a41567 <dymy827@gmail.com>
2023-03-23 08:54:50 +0000

    a

---
+++ M10_ProjetoDeAvaliacao/Compra.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M10_ProjetoDeAvaliacao
+{
+    public class Compra
+    {
+        public Cliente comprador { get; set; }
+        public Produto produto { get; set; }
+        public DateTime dataCompra { get; set; }
+        public string nome { get; set; }
+    }
+}
